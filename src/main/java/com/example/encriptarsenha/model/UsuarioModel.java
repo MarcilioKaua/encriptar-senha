e80891e208648a87f526e104b4e3b9e991d7da42
MarcilioKaua <marciliokaua12@gmail.com>
2022-12-11 11:34:52 -0300

    First commit

---
+++ src/main/java/com/example/encriptarsenha/model/UsuarioModel.java
@@ -0,0 +1,20 @@
+package com.example.encriptarsenha.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name="Usuario")
+public class UsuarioModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(unique = true)
+    private String login;
+    private String password;
+}
---
+++ src/test/java/com/example/encriptarsenha/EncriptarSenhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.encriptarsenha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncriptarSenhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
